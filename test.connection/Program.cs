b487c87ce09ce0c134c5bbaca373a6932fc7bf02
eeschenbach@chroniccareiq.com <9611418+ericraider33@users.noreply.github.com>
2021-03-02 21:46:52 -0500

    Created project and a test project to verify connectivity

---
+++ test.connection/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ServiceModel;
+using kareo.wsdl.KareoServices;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BasicHttpsBinding binding = new BasicHttpsBinding();
+            EndpointAddress endpointAddress = new EndpointAddress("https://webservice.kareo.com/services/soap/2.1/KareoServices.svc");
+            KareoServicesClient client = new KareoServicesClient(binding, endpointAddress);
+            using (client)
+            {
+                RequestHeader requestHeader = new RequestHeader
+                {
+                    User = "xxx", 
+                    Password = "yyyy", 
+                    CustomerKey = "1234"
+                };
+                GetProvidersResp resp = client.GetProviders(new GetProvidersReq { RequestHeader = requestHeader });
+                Console.WriteLine("Found {0} providers", resp.Providers.Length);
+                foreach (ProviderData provider in resp.Providers)
+                {
+                    Console.WriteLine("Provider = {0} {1}", provider.FirstName, provider.LastName);
+                }
+                Console.WriteLine(resp);
+            }
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
